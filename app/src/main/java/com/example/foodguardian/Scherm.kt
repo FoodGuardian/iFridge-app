b854c4310dafdfe1ceca5ed62e8029423e5a7786
Kjeld-H <Kjeld.Heemskerk@student.nhlstenden.com>
2023-05-12 11:42:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/foodguardian/Scherm.kt
@@ -0,0 +1,11 @@
+package com.example.foodguardian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Scherm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scherm)
+    }
+}
