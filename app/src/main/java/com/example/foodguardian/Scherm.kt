162c33c180ddb247b84c2349d3ac948adeb1692f
Kjeld-H <Kjeld.Heemskerk@student.nhlstenden.com>
2023-05-12 16:10:47 +0200

    added menu with icons

--- app/src/main/java/com/example/foodguardian/Scherm.kt
+++ app/src/main/java/com/example/foodguardian/Scherm.kt
@@ -2,10 +2,20 @@ package com.example.foodguardian
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class Scherm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scherm)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+
+        findViewById<View>(R.id.imageMenudropdown).setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
     }
 }
