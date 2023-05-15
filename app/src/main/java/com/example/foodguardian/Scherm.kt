04cda91c2e191a93cebcefc387a424a23932a01f
Kjeld-H <Kjeld.Heemskerk@student.nhlstenden.com>
2023-05-15 10:02:24 +0200

    Added connection check in app

--- app/src/main/java/com/example/foodguardian/Scherm.kt
+++ app/src/main/java/com/example/foodguardian/Scherm.kt
@@ -3,13 +3,25 @@ package com.example.foodguardian
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 
 class Scherm : AppCompatActivity() {
+
+    private lateinit var cld : ConnectionCheck
+
+    private lateinit var layoutToolBarWithNetwork : ConstraintLayout
+    private lateinit var layoutToolBarWithNoNetwork : ConstraintLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scherm)
+
+        checkNetworkConnection()
+
+        layoutToolBarWithNetwork = findViewById(R.id.layoutToolBarWithNetwork)
+        layoutToolBarWithNoNetwork = findViewById(R.id.layoutToolBarWithNoNetwork)
+
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
 
@@ -18,4 +30,18 @@ class Scherm : AppCompatActivity() {
         }
 
     }
+    private fun checkNetworkConnection(){
+       cld = ConnectionCheck(application)
+
+       cld.observe(this, { isConnected ->
+           if (isConnected) {
+               layoutToolBarWithNetwork.visibility = View.VISIBLE
+               layoutToolBarWithNoNetwork.visibility = View.GONE
+           } else {
+                layoutToolBarWithNetwork.visibility = View.GONE
+               layoutToolBarWithNoNetwork.visibility = View.VISIBLE
+           }
+       })
+   }
+
 }
